3661606e96af8ed23b8b2ac1e6694a0136dda981
NandanxD <nandan.ts@dr-ait.org>
2021-04-17 13:06:21 +0530

    Renamed activity_main.xml to MAuthentication Renamed activity_main2.xml to Genre

--- app/src/main/java/com/furrycatxd/newsx/MAuthentication.java
+++ app/src/main/java/com/furrycatxd/newsx/MAuthentication.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MAuthentication extends AppCompatActivity {
     String api_key="115e4b65479f43f29bbdb75f1d9047c0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mauthentication);
 
     }
 }
