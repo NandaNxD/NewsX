a614589fb4a3e2729030dd4ce20fb05b3cd58af1
NandanxD <nandan.ts@dr-ait.org>
2021-04-17 12:04:27 +0530

    First Commit

---
+++ app/src/main/java/com/furrycatxd/newsx/MainActivity.java
@@ -0,0 +1,16 @@
+package com.furrycatxd.newsx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    String api_key="115e4b65479f43f29bbdb75f1d9047c0";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
