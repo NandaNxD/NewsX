0051d38594b0e451fcf36abe8c7fe7ae8324b29b
NandanxD <nandan.ts@dr-ait.org>
2021-04-17 22:01:24 +0530

    Added GoogleSignIn Authentication in MAuthntication
    Checks if already signedin:
    	if signedin:
    		checkfor email
    			if email is present in database:
    				go to mainactivity where news is
    				displayed
    			else:
    			if email is not present in database:
    				go to genre activity and prompt
    				user to select the different genre
    				that he/she likes
    	else:
    		stay in MAuthentication Screen

--- app/src/main/java/com/furrycatxd/RocketNews/MAuthentication.java
+++ app/src/main/java/com/furrycatxd/RocketNews/MAuthentication.java
@@ -2,15 +2,93 @@ package com.furrycatxd.RocketNews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import static android.content.ContentValues.TAG;
 
 public class MAuthentication extends AppCompatActivity {
-    String api_key="115e4b65479f43f29bbdb75f1d9047c0";
+    SignInButton signInButton;
+    int RC_SIGN_IN=0;
+    String database_email;
+    GoogleSignInClient mGoogleSignInClient;
+    Boolean exisiting_google_signin;
 
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            Intent intent=new Intent(this,Genre.class);
+            startActivity(intent);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+
+        }
+    }
+
+    public void alreadySignedIn(){
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account!=null){
+            Intent intent;
+            if(database_email!=null){
+                intent = new Intent(this, MainActivity.class);
+            }
+            else{
+                intent = new Intent(this, Genre.class);
+            }
+            startActivity(intent);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mauthentication);
+        signInButton=findViewById(R.id.sign_in_button);
+        alreadySignedIn();
+        signInButton.setOnClickListener(v -> {
+            switch (v.getId()){
+                case R.id.sign_in_button:
+                    signIn();
+                    break;
+            }
+        });
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
     }
 }
--- app/src/main/java/com/furrycatxd/RocketNews/MainActivity.java
+++ app/src/main/java/com/furrycatxd/RocketNews/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    String api_key="115e4b65479f43f29bbdb75f1d9047c0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
