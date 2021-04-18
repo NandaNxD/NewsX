c3fc5947f8a9e02b191fab06a036f68a8e7dbe28
NandanxD <nandan.ts@dr-ait.org>
2021-04-18 12:34:50 +0530

    Added
    mauthentication:Login Screen Animation Rocket
    genre:handled backpress
    animation files added to R.anim
    gradient added, aimed for mainactivity

--- app/src/main/java/com/furrycatxd/RocketNews/Genre.java
+++ app/src/main/java/com/furrycatxd/RocketNews/Genre.java
@@ -1,14 +1,33 @@
 package com.furrycatxd.RocketNews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActionBarContextView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class Genre extends AppCompatActivity {
+    int backpresscount=0;
+    protected View content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.genre);
+        content=findViewById(android.R.id.content);
+    }
+    @Override
+    public void onBackPressed() {
+        backpresscount++;
+        if(backpresscount%2==0){
+            finish();
+        }
+        else{
+            Snackbar.make(content,"Press again to exit",Snackbar.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/com/furrycatxd/RocketNews/MAuthentication.java
+++ app/src/main/java/com/furrycatxd/RocketNews/MAuthentication.java
@@ -3,11 +3,16 @@ package com.furrycatxd.RocketNews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -15,56 +20,81 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.shobhitpuri.custombuttons.GoogleSignInButton;
 
 import static android.content.ContentValues.TAG;
 
 public class MAuthentication extends AppCompatActivity {
-    SignInButton signInButton;
     int RC_SIGN_IN=0;
     String database_email;
+    TextView textView;
     GoogleSignInClient mGoogleSignInClient;
-    Boolean exisiting_google_signin;
+    LottieAnimationView lottieAnimationView;
+    SignInButton signInButton;
 
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent,RC_SIGN_IN);
+        //overridePendingTransition(R.anim.slide_in_up,R.anim.slide_out_up);
     }
+
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
             // Signed in successfully, show authenticated UI.
             Intent intent=new Intent(this,Genre.class);
+            lottieAnimationView.pauseAnimation();
+            lottieAnimationView.cancelAnimation();
             startActivity(intent);
+            finish();
+            finishAndRemoveTask();
+            //overridePendingTransition(R.anim.slide_in_up,R.anim.slide_out_up);
+
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+//            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
 
         }
     }
 
-    public void alreadySignedIn(){
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if(account!=null){
-            Intent intent;
-            if(database_email!=null){
-                intent = new Intent(this, MainActivity.class);
-            }
-            else{
-                intent = new Intent(this, Genre.class);
-            }
-            startActivity(intent);
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mauthentication);
-        signInButton=findViewById(R.id.sign_in_button);
-        alreadySignedIn();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        if(account!=null){
+            Intent intent;
+            if(database_email!=null){
+                intent = new Intent(getApplicationContext(), MainActivity.class);
+            }
+            else{
+                intent = new Intent(getApplicationContext(), Genre.class);
+            }
+            startActivity(intent);
+            finish();
+            finishAndRemoveTask();
+        }
+
+        signInButton=findViewById(R.id.signInButton);
+        lottieAnimationView=findViewById(R.id.lottieAnimation);
+        lottieAnimationView.playAnimation();
         signInButton.setOnClickListener(v -> {
             switch (v.getId()){
-                case R.id.sign_in_button:
+                case R.id.signInButton:
                     signIn();
                     break;
             }
@@ -74,21 +104,50 @@ public class MAuthentication extends AppCompatActivity {
                 .requestEmail()
                 .build();
 
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mGoogleSignInClient = GoogleSignIn.getClient(getApplicationContext(), gso);
 
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+//                if(account!=null){
+//                    Intent intent;
+//                    if(database_email!=null){
+//                        intent = new Intent(getApplicationContext(), MainActivity.class);
+//                    }
+//                    else{
+//                        intent = new Intent(getApplicationContext(), Genre.class);
+//                    }
+//                    startActivity(intent);
+//                    finish();
+//                    finishAndRemoveTask();
+//                }
+//
+//                signInButton=findViewById(R.id.signInButton);
+//                lottieAnimationView=findViewById(R.id.lottieAnimation);
+//                lottieAnimationView.playAnimation();
+//                signInButton.setOnClickListener(v -> {
+//                    switch (v.getId()){
+//                        case R.id.signInButton:
+//                            signIn();
+//                            break;
+//                    }
+//                });
+//
+//                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                        .requestEmail()
+//                        .build();
+//
+//                mGoogleSignInClient = GoogleSignIn.getClient(getApplicationContext(), gso);
+//
+//            }
+//        }).start();
+//
+//
+//
     }
 
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
 }
